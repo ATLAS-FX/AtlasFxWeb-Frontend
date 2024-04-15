3401bb7c55945da0c985235ad97b47815ec53146
Alfafis <ctpmmg2009_101@hotmail.com>
2024-04-15 09:43:15 -0300

    inital flow login

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,11 +1,12 @@
 import Login from '@/pages/Auth/Login'
 import React from 'react'
 import { Navigate, Routes as ReactRoutes, Route } from 'react-router-dom'
+import Atlas_Logo from '../assets/atlas_logo.png'
 
 export const AuthRoutes: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-start overflow-x-hidden p-16 gap-14">
-      <img src="../src/assets/atlas_logo.png" alt="logo_atlas" width={200} />
+    <div className="flex flex-col items-center justify-start gap-14 overflow-x-hidden p-16">
+      <img src={Atlas_Logo} alt="logo_atlas" width={200} />
       <ReactRoutes>
         <Route path="/" Component={Login} />
         <Route path="/login" Component={Login} />
