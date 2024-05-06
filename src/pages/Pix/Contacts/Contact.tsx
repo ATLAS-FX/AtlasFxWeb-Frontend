963d2e5af0bb0d07ba71597c446c560343941444
Alfafis <ctpmmg2009_101@hotmail.com>
2024-05-06 11:39:32 -0300

    update contact pix

--- src/pages/Pix/Contacts/Contact.tsx
+++ src/pages/Pix/Contacts/Contact.tsx
@@ -7,20 +7,40 @@ import {
   TooltipProvider,
   TooltipTrigger
 } from '@/components/ui/tooltip'
+import { toast } from '@/components/ui/use-toast'
 import { Dispatch, SetStateAction } from 'react'
 
 interface IContact {
   name: string
+  keyPix: string
   openModal: Dispatch<SetStateAction<boolean>>
-  step: Dispatch<SetStateAction<number>>
 }
 
-const Contact: React.FC<IContact> = ({ name, openModal, step }) => {
+const Contact: React.FC<IContact> = ({ name, keyPix, openModal }) => {
+  const handleCopyClick = (value: string) => {
+    navigator.clipboard
+      .writeText(value)
+      .then(() => {
+        toast({
+          variant: 'success',
+          title: 'Chave copiada com sucesso',
+          description: ''
+        })
+      })
+      .catch((e: Error) => {
+        toast({
+          variant: 'destructive',
+          title: 'Falha ao copiar chave',
+          description: e.message
+        })
+      })
+  }
+
   return (
     <div className="flex items-center justify-between border-b-2 border-colorPrimary-500 py-2 ">
       <Button
         className="flex cursor-pointer items-center gap-2 fill-colorPrimary-500 text-lg font-semibold text-colorPrimary-500 hover:fill-colorSecondary-500 hover:text-colorSecondary-500"
-        onClick={() => step(1)}
+        onClick={() => handleCopyClick(keyPix)}
         variant="ghost"
       >
         <IconUser size={24} />
--- src/pages/Pix/Transactions/PixSuccess.tsx
+++ src/pages/Pix/Transactions/PixSuccess.tsx
@@ -22,6 +22,28 @@ const PixSuccess: React.FC<IPixSuccess> = ({
   time,
   amount
 }) => {
+  // const handleDownloadPDF = () => {
+  //   const bodyPdf = (
+  //     <PdfDefault
+  //       amount={amount}
+  //       name={name}
+  //       ag={ag}
+  //       bank={bank}
+  //       cont={cont}
+  //       time={time}
+  //       transaction={transaction}
+  //     />
+  //   )
+  //   // Specify the id of the element you want to convert to PDF
+  //   html2canvas(bodyPdf).then((canvas) => {
+  //     const imgData = canvas.toDataURL('image/png')
+  //     const pdf = new jsPDF()
+  //     pdf.addImage(imgData, 'PNG', 0, 0)
+  //     pdf.save(`comprovante-${time}.pdf`)
+  //     // Specify the name of the downloaded PDF file
+  //   })
+  // }
+
   return (
     <>
       <h4 className="flex items-center gap-2 text-sm font-semibold">
@@ -60,7 +82,7 @@ const PixSuccess: React.FC<IPixSuccess> = ({
       <Separator className="bg-colorPrimary-500" />
       <Button
         className="w-full rounded-md bg-[#008000]"
-        onClick={() => alert('Download começará em breve!!!')}
+        //  onClick={handleDownloadPDF}
       >
         Baixar comprovante em PDF
       </Button>
